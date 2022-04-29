17145c09d2b0bd219bfd841a689c024217d78da7
zDridi <dridi.zied@courrier.uqam.ca>
2022-04-29 14:19:05 -0400

    Initial : structure minimale du projet webapp : SpringBoot 2.6.7 , Dependencies (Spring Web, Thymeleaf, Lombok)

---
+++ src/test/java/com/dridi/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dridi.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
