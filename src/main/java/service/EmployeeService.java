ef27d4d1fb1e4455ed7267ad612c221593be479c
zDridi <dridi.zied@courrier.uqam.ca>
2022-04-30 15:29:31 -0400

    C-9 | Ajout du service EmployeeService

---
+++ src/main/java/service/EmployeeService.java
@@ -0,0 +1,47 @@
+package service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import model.Employee;
+import repository.EmployeeProxy;
+
+import lombok.Data;
+
+@Data
+@Service
+public class EmployeeService {
+
+    @Autowired
+    private EmployeeProxy employeeProxy;
+
+    public Employee getEmployee(final int id) {
+        return employeeProxy.getEmployee(id);
+    }
+
+    public Iterable<Employee> getEmployees() {
+        return employeeProxy.getEmployees();
+    }
+
+    public void deleteEmployee(final int id) {
+        employeeProxy.deleteEmployee(id);
+    }
+
+    public Employee saveEmployee(Employee employee) {
+        Employee savedEmployee;
+
+        // Functional rule : Last name must be capitalized.
+        employee.setLastName(employee.getLastName().toUpperCase());
+
+        if(employee.getId() == null) {
+            // If id is null, then it is a new employee.
+            savedEmployee = employeeProxy.createEmployee(employee);
+        } else {
+            savedEmployee = employeeProxy.updateEmployee(employee);
+        }
+
+        return savedEmployee;
+    }
+
+}
