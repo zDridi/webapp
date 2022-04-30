1473dd5e78f2bfdb4012ca7afc7de044f3bacab4
zDridi <dridi.zied@courrier.uqam.ca>
2022-04-30 15:18:22 -0400

    C-8 | Implémentation de la communication entre l'application web et l'API REST

---
+++ src/main/java/repository/EmployeeProxy.java
@@ -0,0 +1,131 @@
+package repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import com.dridi.webapp.CustomProperties;
+import model.Employee;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class EmployeeProxy {
+
+    @Autowired
+    private CustomProperties props;
+
+    /**
+     * Get all employees
+     * @return An iterable of all employees
+     */
+
+    public Iterable<Employee> getEmployees() {
+        String baseApiUrl = props.getApiUrl();                  // récupère l’URL de l’API.
+        String getEmployeesUrl = baseApiUrl + "/employees";     // complète l’URL de l’API par le path de l'endpoint à joindre
+
+        // RestTemplate permet d’exécuter une requête HTTP. On a donc besoin de fournir l’URL, le type de requête (GET, POST, etc.), et le type d’objet qui sera retourné.
+        // RestTemplate non seulement fait la requête à l’API mais en plus convertit le résultat JSON en objet Java
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Iterable<Employee>> response = restTemplate.exchange(
+                getEmployeesUrl,        // l’URL ;
+                HttpMethod.GET,         // la méthode HTTP (grâce à l’enum HttpMethod)
+                null,                   // Null en lieu et place d’un objet HttpEntity, ainsi on laisse un comportement par défaut
+                new ParameterizedTypeReference<Iterable<Employee>>() {}     // si l’endpoint renvoie un objet simple, alors il suffira d’indiquer <Object>.class, d'ou l'utilisation de ParameterizedTypeReference
+        );
+
+        log.debug("Get Employees call " + response.getStatusCode().toString());
+
+        return response.getBody();      // récupère notre objet Iterable<Employee> grâce à la méthode getBody() de l’objet Response.
+    }
+
+    /**
+     * Get an employee by the id
+     * @param id The id of the employee
+     * @return The employee which matches the id
+     */
+    public Employee getEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String getEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                getEmployeeUrl,
+                HttpMethod.GET,
+                null,
+                Employee.class
+        );
+
+        log.debug("Get Employee call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Add a new employee
+     * @param e A new employee (without an id)
+     * @return The employee full filled (with an id)
+     */
+    public Employee createEmployee(Employee e) {
+        String baseApiUrl = props.getApiUrl();
+        String createEmployeeUrl = baseApiUrl + "/employee";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Employee> request = new HttpEntity<Employee>(e);
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                createEmployeeUrl,
+                HttpMethod.POST,
+                request,
+                Employee.class);
+
+        log.debug("Create Employee call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Update an employee - using the PUT HTTP Method.
+     * @param e Existing employee to update
+     */
+    public Employee updateEmployee(Employee e) {
+        String baseApiUrl = props.getApiUrl();
+        String updateEmployeeUrl = baseApiUrl + "/employee/" + e.getId();
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<Employee> request = new HttpEntity<Employee>(e);
+        ResponseEntity<Employee> response = restTemplate.exchange(
+                updateEmployeeUrl,
+                HttpMethod.PUT,
+                request,
+                Employee.class);
+
+        log.debug("Update Employee call " + response.getStatusCode().toString());
+
+        return response.getBody();
+    }
+
+    /**
+     * Delete an employee using exchange method of RestTemplate
+     * instead of delete method in order to log the response status code.
+     * @param : e The employee to delete
+     */
+    public void deleteEmployee(int id) {
+        String baseApiUrl = props.getApiUrl();
+        String deleteEmployeeUrl = baseApiUrl + "/employee/" + id;
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Void> response = restTemplate.exchange(
+                deleteEmployeeUrl,
+                HttpMethod.DELETE,
+                null,
+                Void.class);
+
+        log.debug("Delete Employee call " + response.getStatusCode().toString());
+    }
+
+}
