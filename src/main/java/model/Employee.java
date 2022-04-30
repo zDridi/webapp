ac4e7c92cecb97f7d4f7a36191c66c063655812f
zDridi <dridi.zied@courrier.uqam.ca>
2022-04-30 11:25:16 -0400

    C-5 | Ajout de la classe domaine Employee

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,18 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String mail;
+
+    private String password;
+
+}
